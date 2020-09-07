7b46d629fcd05b61cea4ade66fb17cdd1347ea85
Pedro Augusto <pams.pedro@gmail.com>
2020-09-07 19:44:00 -0300

    feat: add support to tailwind from tailwindcss preset

--- src/PresetServiceProvider.php
+++ src/PresetServiceProvider.php
@@ -5,6 +5,7 @@ namespace Laravelha\Web;
 use Illuminate\Console\Command;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Ui\UiCommand;
+use Symfony\Component\Process\Process;
 
 class PresetServiceProvider extends ServiceProvider
 {
@@ -19,19 +20,45 @@ class PresetServiceProvider extends ServiceProvider
 
             $options = $command->option('option');
 
-            if($command->option('auth')) {
-                $command->call('ui', ['type' => 'bootstrap', '--auth' => true, '--quiet' => true]);
-            }
-
-            Presets\Bootstrap::install($command->option('auth'));
-
-            if($this->withDatatables($options)) {
-                Presets\Datatable::install();
+            if(! $this->withTailwind($options)) {
+                $this->bootstrap($command, $options);
+            } else {
+                $this->tailwind($command);
             }
 
             $command->info('Laravelha scaffolding installed successfully.');
             $command->comment('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
         });
+    }
+
+    /**
+     * @param Command $command
+     * @param array $options
+     */
+    private function bootstrap(Command $command, array $options)
+    {
+        if($command->option('auth')) {
+            $command->call('ui', ['type' => 'bootstrap', '--auth' => true, '--quiet' => true]);
+        }
+
+        Presets\Bootstrap::install($command->option('auth'));
+
+        if($this->withDatatables($options)) {
+            Presets\Datatable::install();
+        }
+    }
+
+    /**
+     * @param Command $command
+     */
+    private function tailwind(Command $command)
+    {
+        (new Process(['composer', 'require', 'laravel-frontend-presets/tailwindcss^4.2.0 --dev'], base_path()))
+            ->setTimeout(null)
+            ->run();
+
+
+        $command->call('ui', ['type' => 'tailwindcss', '--auth' => $command->option('auth'), '--quiet' => true]);
     }
 
     /**
@@ -41,5 +68,14 @@ class PresetServiceProvider extends ServiceProvider
     private function withDatatables(array $options)
     {
         return in_array('datatables', $options);
+    }
+
+    /**
+     * @param array $options
+     * @return bool
+     */
+    private function withTailwind(array $options)
+    {
+        return in_array('tailwindcss', $options);
     }
 }
